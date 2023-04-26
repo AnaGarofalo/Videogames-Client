eed2c8caf543585f548ebea525acf3eb6f13e3af
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-04-26 19:07:19 -0300

    deploy

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -1,2 +1,2 @@
-// export const URL_BASE = "https://videogames-api-bom9.onrender.com";
-export const URL_BASE = "http://localhost:3001";
+export const URL_BASE = "https://videogames-api-bom9.onrender.com";
+// export const URL_BASE = "http://localhost:3001";
