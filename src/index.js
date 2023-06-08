c4174c08a155999581938e552aaddfcbb4ae621e
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-06-08 18:57:17 -0300

    changed api deploy to railway

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter, HashRouter, Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
 
   document.getElementById("root")
