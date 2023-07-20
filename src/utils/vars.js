c65ce8ab887c88aae386eafbb5fe707c739c0fb9
Ana María Garófalo <119868532+AnaGarofalo@users.noreply.github.com>
2023-07-20 11:02:08 -0300

    Update deploy back to render

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -1,3 +1,3 @@
-// export const URL_BASE = "https://videogames-api-bom9.onrender.com";
+export const URL_BASE = "https://videogames-api-bom9.onrender.com";
 // export const URL_BASE = "http://localhost:3001";
-export const URL_BASE = "https://videogames-api-production.up.railway.app";
+// export const URL_BASE = "https://videogames-api-production.up.railway.app";
