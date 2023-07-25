c1039e566657eee038c779e441fb3158c54696fd
Ana María Garófalo <119868532+AnaGarofalo@users.noreply.github.com>
2023-07-25 13:47:26 -0300

    deploy to railway

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -1,5 +1,4 @@
 // export const URL_BASE = "https://videogames-api-bom9.onrender.com";
 // export const URL_BASE = "http://localhost:3001";
-// export const URL_BASE = "https://videogames-api-production.up.railway.app";
+export const URL_BASE = "https://videogames-api-production-580e.up.railway.app";
 
-export const URL_BASE="https://videojuegos-minimalista.onrender.com"
